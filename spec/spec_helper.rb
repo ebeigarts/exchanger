a68f0e6493ccad225977fe76be03ad4bf4af7a03
Chad Schroeder <schroeder@surgery.wisc.edu>
2016-05-06 13:32:04 -0500

    Update VCR version and encode filter_sensitive_data values

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,6 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.ignore_localhost = true
   c.default_cassette_options = { :record => :new_episodes }
-  c.filter_sensitive_data('FILTERED_USERNAME') { Exchanger.config.username.gsub("@", "%40") }
-  c.filter_sensitive_data('FILTERED_PASSWORD') { Exchanger.config.password }
+  c.filter_sensitive_data('FILTERED_USERNAME') { CGI::escape(Exchanger.config.username) }
+  c.filter_sensitive_data('FILTERED_PASSWORD') { CGI::escape(Exchanger.config.password) }
 end
